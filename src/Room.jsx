435d9502ff5107089e570da9b738d549a7fde8f1
tahir-fasterhq <158481190+tahir-fasterhq@users.noreply.github.com>
2024-02-20 23:24:11 +0500

    first commit...

--- src/Room.jsx
+++ src/Room.jsx
@@ -6,20 +6,22 @@ const Room = () => {
   const { roomId } = useParams();
   const elementRef = useRef(null); // Creating a ref
 
-  useEffect(() => {
-    if (elementRef.current) {
-      const myMeeting = async () => {
+  
+//   useEffect(() => {
+    // if (elementRef.current) {
+      const myMeeting = async (element) => {
         const appId = 1260205419;
         const serverSecret = "2fb698cb6b586694629ce2066f3de77e";
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
           appId,
           serverSecret,
           roomId,
+          Date.now().toString(),
           "Sigma Developer"
         );
         const zp = ZegoUIKitPrebuilt.create(kitToken);
         zp?.joinRoom({
-          container: elementRef.current,
+          container: element,
           sharedLinks: [
             {
               name: "Share Link",
@@ -30,14 +32,16 @@ const Room = () => {
             mode: ZegoUIKitPrebuilt.OneONoneCall,
           },
         });
+        console.log(zp);
       };
-      myMeeting();
-    }
-  }, [roomId]); // Added roomId to the dependencies array
+    //   myMeeting();
+    // }
+//   }, [roomId]); // Added roomId to the dependencies array
 
   return (
     <div>
-      <div ref={elementRef} style={{ width: "100vw", height: "100vh" }}></div>
+      <div ref={myMeeting} style={{ width: "100vw", height: "100vh" }}></div>
+      <div>{roomId} </div>
     </div>
   );
 };
